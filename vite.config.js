777f941c2220c32407a45cc574f69beed3aa7e10
unknown <olewuenyie@gmail.com>
2026-01-28 05:41:45 +0100

    New Fix

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
-    server: {
-    allowedHosts: [
-      'unsoaring-sharice-unsplendorously.ngrok-free.dev'
-    ]
-  }
-})
+  plugins: [tailwindcss(), react()],
+  server: {
+    allowedHosts: ["unsoaring-sharice-unsplendorously.ngrok-free.dev"],
+  },
+  base: process.env.VITE_BASE_PATH || "/Website_portfolio",
+});
